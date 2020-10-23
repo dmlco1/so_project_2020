fc5c076c927f8287c387b387b19f3a44611eb482
Duarte Miguel Leite Casaleiro <a92697@iscte.pt>
2020-10-23 17:17:14 +0100

    Versao Final 2.0

--- cria_pacientes.sh
+++ cria_pacientes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 file=/etc/passwd
 if [ -f $file ] ; then
-    cat $file | awk '/^a[0-9+]/' | head | awk -F':' '{print $1";"$5}' | awk -F';' '{print $1";"$2}' | awk -F';' '{print $1";"$2$3$4$5$1"@iscte-iul.pt"";""100"}' | sed 's/,/;/g' | sed 's/^a//' > pacientes.txt
+    cat $file | awk '/^a[0-9+]/' | head | awk -F':' '{print $1";"$5}' | awk -F';' '{print $1";"$2$3$4$5$1"@iscte-iul.pt"";""100"}' | sed 's/,/;/g' | sed 's/^a//' > pacientes.txt
 else
     echo "O ficheiro não existe"
 fi
