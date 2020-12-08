4a41d5ce118912179aea3923efb97bc480d777d8
Duarte Miguel Leite Casaleiro <a92697@iscte.pt>
2020-12-08 17:10:44 +0000

    Functional for 1 request. Missing s4) and s5)

--- parte_3/cliente.c
+++ parte_3/cliente.c
@@ -1,10 +1,27 @@
 #include "defines.h"
 
+Mensagem m;
+int msg_queue_id, msg_queue_status, acabar, iniciar;
+
+//void cancelar_espera(Mensagem msg);
+
+void cancelar_espera(){
+    printf("Paciente cancelou o pedido\n");
+	
+	//ligar a message queue do servidor dedicado
+    msg_queue_id = msgget ( IPCS_KEY, 0600 | IPC_CREAT ); 
+    exit_on_error (msg_queue_id, "Erro a ligar a mssage queue");
+	
+	m.Consulta.status = 5;
+	
+	msg_queue_status = msgsnd(msg_queue_id, &m, sizeof(m), 0);
+    exit_on_error (msg_queue_status, "Recepção");
+
+    exit(0);
+}
+
 void main(){
 
-	Mensagem m;
-	int msg_queue_id, msg_queue_status;
-	
 	char tipo[10];
 
 	// c1) c2) Introduzir os dados de consulta
@@ -30,7 +47,42 @@ void main(){
 	msg_queue_status = msgsnd(msg_queue_id, &m, sizeof(m), 0); 
 	exit_on_error(msg_queue_status, "Erro de envio");
 
+	//c7)
+    signal(SIGINT, cancelar_espera);
 
+	while(acabar == 0){
+		//Receber mensagem
+		msg_queue_status = msgrcv(msg_queue_id, &m, sizeof(m), getpid(), 0);
+		exit_on_error (msg_queue_status, "Recepção");
+	
+		//c4) c5) c6)	
+		switch(m.Consulta.status){
+        case 2: iniciar = 1;printf("Consulta iniciada para o processo %d\n", m.Consulta.pid_consulta);break;
+        case 3: (iniciar) ? printf("Consulta terminada para o processo %d\n", m.Consulta.pid_consulta) : perror("Erro");exit(0);break;
+		case 4: printf("Consulta nao e possivel para o processo %d\n", m.Consulta.pid_consulta);exit(0);break;
+        default: perror("ERRO"); exit(0);
+        }
 
+	}
 
 }
+
+/*
+void cancelar_espera(Mensagem msg){
+    printf("Paciente cancelou o pedido\n");
+
+    //ligar a message queue do servidor dedicado
+    msg_queue_id = msgget ( IPCS_KEY, 0600 | IPC_CREAT );
+    exit_on_error (msg_queue_id, "Erro a ligar a mssage queue");
+
+    msg.Consulta.status = 5;
+
+    msg_queue_status = msgsnd(msg_queue_id, &m, sizeof(m), 0);
+    exit_on_error (msg_queue_status, "Recepção");
+
+    exit(0);
+}
+*/
+
+
+
--- parte_3/servidor.c
+++ parte_3/servidor.c
@@ -5,34 +5,38 @@
 
 Mensagem m;
 Mensagem *lista_consultas;
+int *indice_lista_consultas = NULL;
 
-int countTipo1, countTipo2, countTipo3, countPerdidas;
+int countTipo1 = 0;
+int countTipo2, countTipo3, countPerdidas;
 int iniciar, acabar;
 int id, status, msg_queue_id, msg_queue_status;
 
+void verificar_vaga();
+
 void main(){	
+	indice_lista_consultas = malloc(sizeof(int));
 	//Check if shared memory already exists
 	if (shmget(IPCS_KEY, TAMANHO * sizeof(Mensagem), IPC_CREAT | IPC_EXCL | 0600) > 0){
 		iniciar = 1;
 	}
 
+	//Ligar a shared memory
 	id = shmget(IPCS_KEY, TAMANHO * sizeof(Mensagem), IPC_CREAT| 0600);
     Mensagem *lista_consultas = (Mensagem*)shmat ( id, 0, 0 ); exit_on_null (lista_consultas, "Attach");
 	
 	//Se e a primeira vez a ser criada, inicializamos o array
-	if(check()){
-		printf("Como nao exsitia iniciamos\n");
+	if(iniciar){
 		for(int i = 0; i < TAMANHO; i++){
-			lista_consultas[i].Consulta.tipo = 1;
-			printf("TIPOOOO %d\n", lista_consultas[i].Consulta.tipo);
+			lista_consultas[i].Consulta.tipo = -1;
 	     }
+		countTipo1 = 0; countTipo2 = 0; countTipo3 = 0; //colocar os counts a 0
 		printf("Shared memory iniciada!\n");
 	}
 	
 	//Ligar a message queue	
 	msg_queue_id = msgget(IPCS_KEY, IPC_CREAT | 0600); 
 	exit_on_error( msg_queue_id, "Erro a criar message queue");
-	printf("AQUI\n");
 	
 	//Ficar a espera de mensagens
 	while(acabar == 0){
@@ -42,12 +46,70 @@ void main(){
 
         printf ("Chegou um novo pedido de consulta do tipo <%d>, descricao <%s> e PID <%d>\n", m.Consulta.tipo, m.Consulta.descricao, m.Consulta.pid_consulta);
 		
-		
-		
-		//pause();
+		//fork
+		int n = fork();
+		if(n == 0){			
+			int vaga;
+			for(int i = 0; i < TAMANHO; i++){
+				if((lista_consultas[i]).Consulta.tipo == -1){
+					*indice_lista_consultas = i;
+					vaga = 1;
+				}
+            }
+			//Se tem vaga coloca a consulta no indice - Comeca no fim e vai para o inicio!
+			if(vaga){
+				lista_consultas[*indice_lista_consultas] = m;
+                printf("Consulta agendada para a sala <%d>\n", *indice_lista_consultas);
+				
+				//incrementar o respetivo contador
+				switch(m.Consulta.tipo){
+				case 1: countTipo1++;break;
+				case 2: countTipo2++;break;
+				case 3: countTipo3++;break;
+				default: perror("ERRO"); exit(0);
+				}
+				
+				m.tipo = m.Consulta.pid_consulta;
+                m.Consulta.status = 2;
+                
+                //Mandar mensagem tipo 2-Iniciada para o cliente
+                msg_queue_status = msgsnd(msg_queue_id, &m, sizeof(m), 0);
+                exit_on_error(msg_queue_status, "Erro de envio");
+								
+
+				sleep(DURACAO);
+			
+				printf("Consulta terminada na sala <%d>\n", *indice_lista_consultas);
+				m.tipo = m.Consulta.pid_consulta;
+                m.Consulta.status = 3;
+
+                //Mandar mensagem tipo 3-Terminada para o cliente
+                msg_queue_status = msgsnd(msg_queue_id, &m, sizeof(m), 0);
+                exit_on_error(msg_queue_status, "Erro de envio");
+
+				exit(0);
+			}
+			else{
+				//Se nao houver vagas na lista
+				printf("Lista de consultas cheia\n");
+				countPerdidas++;
+				m.tipo = m.Consulta.pid_consulta;
+				m.Consulta.status = 4;
+				
+				//Mandar mensagem tipo 4-Perdida para o cliente
+				msg_queue_status = msgsnd(msg_queue_id, &m, sizeof(m), 0);
+				exit_on_error(msg_queue_status, "Erro de envio");
+				exit(0);				
+			}
+		}		
+		printf("O pai esta ativo\n");
 	}
 
 }
+
+
+
+
 
 
 
